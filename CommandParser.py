1f2f04525c4c8a594392416f337cfdd0c6c8ae49
Kyle <kylelodden@gmail.com>
2019-02-23 22:53:56 -0500

    converted CommandParser and UserInputParser to singletons

--- CommandParser.py
+++ CommandParser.py
@@ -2,16 +2,31 @@ from UserInputParser import UserInputParser
 
 
 class CommandParser:
+
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if CommandParser.__instance is not None:
+            return CommandParser.__instance
+        else:
+            CommandParser.__instance = CommandParser()
+            return CommandParser.__instance
+
     def __init__(self):
-        self.inputParser = UserInputParser()
-        self.keywords = ["n", "e", "s", "w"]
+        if CommandParser.__instance is not None:
+            raise Exception("attempted another init of a Singleton!")
+        else:
+            self.inputParser = UserInputParser.getInstance()
+            self.keywords = ["n", "e", "s", "w"]
+            CommandParser.__instance = self
 
     def findKeyWords(self, string):
         for word in string.split(" "):
             if word in self.keywords:
                 print("{0} found as a keyword".format(word))
 
-commparse = CommandParser()
+commparse = CommandParser.getInstance()
 
 s = commparse.inputParser.getUserCommand("?")
 
--- UserInputParser.py
+++ UserInputParser.py
@@ -1,22 +1,40 @@
 
 class UserInputParser:
-    def __init__(self):
-        pass
+    """Singleton"""
 
-    def readUserInput(self, prompt):
+    __instance = None
+
+    def __init__(self):
+        if UserInputParser.__instance is not None:
+            raise Exception("attempted another init of a Singleton!")
+        else:
+            UserInputParser.__instance = self
+
+    @staticmethod
+    def getInstance():
+        if UserInputParser.__instance is not None:
+            return UserInputParser.__instance
+        else:
+            UserInputParser.__instance = UserInputParser()
+            return UserInputParser.__instance
+
+    @staticmethod
+    def getUserInput(prompt):
         return input(prompt)
 
-    def getUserInputInteger(self, prompt):
+    @staticmethod
+    def getUserInputInteger(prompt):
         try:
-            return int(self.readUserInput(prompt))
+            return int(UserInputParser.getUserInput(prompt))
 
         except Exception as e:
             print("Sorry, I didn't recognize that, I was expecting an integer")
             return None
 
-    def getUserCommand(self, prompt):
+    @staticmethod
+    def getUserCommand(prompt):
         try:
-            return self.readUserInput(prompt)
+            return UserInputParser.getUserInput(prompt)
 
         except Exception as e:
             print("ERR: could not parse user input.")
